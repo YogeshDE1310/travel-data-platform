3856350a094cb6a15a0c52e91bb8df2ddb848203
Yogesh <yogeshtyagi1310@gmail.com>
2026-06-23 10:41:25 +0530

    Remove unused local silver and gold folders

---
+++ utils/spark_session.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+
+def get_spark_session(app_name):
+
+    spark = (
+        SparkSession.builder
+        .appName(app_name)
+        .getOrCreate()
+    )
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    return spark
